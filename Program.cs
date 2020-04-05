29dc15631f43f24990b268b215c18f55398cee0b
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-04-05 14:49:47 +0530

    refactor changes:- seprate file for seprate class, multiline commenting,use interface

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace cash_counter
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("no of people in queue");
+            int noOfPeopleInQue = Convert.ToInt32(Console.ReadLine());
+            Queue list = new Queue(noOfPeopleInQue);
+
+
+            int count = 0;
+
+            banking bank = new banking();
+            do
+            {
+                int userBalance = 5000;
+                int ubalance = userBalance;
+                int totalBalanceOfUser = 0;
+                Console.WriteLine("enter cash ");
+                int cash = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("please enter 1 for deposit 2 for withdraw");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        totalBalanceOfUser = bank.depositCash(cash, ubalance);
+                        list.enqueue(totalBalanceOfUser);
+                        Console.WriteLine("available balance .." + totalBalanceOfUser);
+                        list.dequeue();
+                        cash = 0;
+                        break;
+
+                    case 2:
+                        if (ubalance < cash)
+                        {
+                            Console.WriteLine("not ifficient cash");
+                        }
+                        else
+                        {
+                            totalBalanceOfUser = bank.withdrawCash(cash, ubalance);
+                            list.enqueue(totalBalanceOfUser);
+                            Console.WriteLine("available balance .." + totalBalanceOfUser);
+                            list.dequeue();
+                            cash = 0;
+                        }
+                        break;
+
+                }
+                count++;
+            } while (count < noOfPeopleInQue);
+            list.display();
+
+        }
+    }
+}
---
+++ queue.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cash_counter
+{
+    public class Queue//queue class 
+    {
+        int front, rear, size;
+        int capacity;
+        int[] array;
+        //queue constructor
+        /*
+         * to initialize attributes
+         */
+        public Queue(int capacity)
+        {
+            this.capacity = capacity;
+            rear = 0;
+            front = 0;
+            size = 0;
+            array = new int[this.capacity];
+        }
+        //check que is full or not
+        public Boolean isFull(Queue queue)
+        {
+            return (queue.size == queue.capacity);
+        }
+        //check queue is empty
+        public Boolean isEmpty(Queue queue)
+        {
+            return (queue.size == 0);
+        }
+        /*add last 
+         * check queue is full then return 0
+         * if que not full then add itome in queue
+         * increse rear and size
+         */
+        public void enqueue(int itome)
+        {
+            if (isFull(this))
+            {
+                return;
+            }
+            else
+            {
+                array[this.rear] = itome;
+                this.rear = this.rear + 1;
+                this.size = size + 1;
+            }
+        }
+        /*remove first
+         * queue is empty then return 0
+         * if no then remove itome
+         * by frount increase position and itom remove by garbage callector
+         */
+        public int dequeue()
+        {
+            if (isEmpty(this))
+            {
+                return 0;
+            }
+            else
+            {
+
+
+                int itome = this.array[this.front];
+                this.front = this.front + 1;
+                this.size = size - 1;
+                return itome;
+            }
+        }
+        public void display()
+        {
+            while (front != rear)
+            {
+                Console.WriteLine(array[front]);
+                front += 1;
+            }
+        }
+    }
+}
+
